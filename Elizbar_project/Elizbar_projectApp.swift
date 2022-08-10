8569a310a1517c7bc16a47e6f247b028ddd3c090
elizbar <elizbar>
2022-08-10 16:56:10 +0400

    Initial Commit

---
+++ Elizbar_project/Elizbar_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elizbar_projectApp.swift
+//  Elizbar_project
+//
+//  Created by alta on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Elizbar_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
